2fb2db1afd32a2b51c867ecf6829ca77e671d9e4
IGiotto <101021686+IGiotto12@users.noreply.github.com>
2024-02-14 13:01:19 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,36 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/*.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if [ -f ListExamples.java ]
+then 
+    echo "Missing ListExamples.java in student submission"
+    echo "Score: 0"
+    exit
+fi 
+
+javac $CPATH
+if []
+then 
+    echo "Compile Error"
+    echo "Score: 0"
+    exit
+fi 
+
+javac -cp $CPATH:. TestListExamples.java
+java -cp $CPATH:.org.junit.runner.JUnitCore TestListExamples > testResult.txt
+
+if [find -Stirng ]
+then 
+    echo "Test"
+    echo 
+
+
+echo "All Tests passed!"
+echo "Scores: 1/1"
